feb992bb249f6a09e0d7f0316d78b8b06abc1392
manishambule0234 <156496342+manishambule0234@users.noreply.github.com>
2025-05-17 12:21:27 +0530

    Time: 35 ms (5.29%), Space: 56.8 MB (75.57%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,58 @@
+class Solution {
+    public static void updatearr(int[]bloomDay,int[]arr,int mid){
+        for(int i=0;i<bloomDay.length;i++){
+            if(bloomDay[i]<=mid)
+            arr[i]=1;
+        }
+    }
+    public static boolean checkvalid(int[] arr,int k, int m){
+        int c=k;
+                int  b=0;
+                for(int ele:arr){
+                    if(ele==1){
+                        c--;
+                    }
+                    else
+                    c=k;
+                    if(c==0) {
+                        b++;
+                        c=k;
+                    }
+                    if(b==m)
+                    return true;
+                }
+                return false;
+    }
+
+
+    public int minDays(int[] bloomDay, int m, int k) {
+        if(bloomDay.length<(long)m*k)
+        return -1;
+        int[] arr=new int[bloomDay.length];
+        Arrays.fill(arr,0);
+        int li=Integer.MAX_VALUE;
+        int ri=0;
+        for(int ele: bloomDay){
+           ri= Math.max(ele,ri);
+           li=Math.min(ele,li);
+        }
+        int flag=0;
+        int ans=Integer.MAX_VALUE;  
+    while(li<=ri){
+            int mid=(li+ri)/2;
+            arr = new int[bloomDay.length]; // reset arr
+            updatearr(bloomDay, arr, mid);
+
+           boolean t= checkvalid(arr,k,m);
+               if(t){
+                ans=Math.min(ans,mid);
+                ri=mid-1;
+                 flag=1;
+               }
+               else
+               li=mid+1;
+            
+        }
+        if(flag==1) return ans;
+        else return -1;
+}}
