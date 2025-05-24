021d551e122a8c1f219c600f43a389ec9c52a0dd
manishambule0234 <156496342+manishambule0234@users.noreply.github.com>
2025-05-24 11:36:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,13 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        double c=1;
+        int m=n;
+        if(m<0) m=m*-1;
+        while(m!=0){
+            c*=x;
+            m--;
+        }
+        
+        return (n<0)? 1/c : c;
     }
 }
