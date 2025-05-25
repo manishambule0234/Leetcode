9c58d334a02e1bb31140b7158e6be77c680df6c5
manishambule0234 <156496342+manishambule0234@users.noreply.github.com>
2025-05-25 15:23:43 +0530

    Time: 168 ms (6.71%), Space: 57.8 MB (67.89%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,31 +1,35 @@
-import java.util.*;
-
 class Solution {
     public int longestPalindrome(String[] words) {
-        Map<String, Integer> map = new HashMap<>();
-        int count = 0;
-        boolean hasCentralWord = false;
-
-        for (String word : words)
-            map.put(word, map.getOrDefault(word, 0) + 1);
-
-        for (String word : map.keySet()) {
-            int freq = map.get(word);
-            if (freq == 0) continue;
-
-            String reversed = new StringBuilder(word).reverse().toString();
-            if (word.equals(reversed)) {
-                count += (freq / 2) * 4;
-                if (freq % 2 == 1) hasCentralWord = true;
-            } else if (map.containsKey(reversed)) {
-                int pairs = Math.min(freq, map.get(reversed));
-                count += pairs * 4;
-                map.put(reversed, 0);
-            }
-            map.put(word, 0);
+        HashMap<String,Integer> HM=new HashMap<>();
+        for(int i=0;i<words.length;i++){
+            HM.put(words[i], HM.getOrDefault(words[i], 0) + 1);
         }
+        int count=0;
+        boolean hasCenter = false;
+        for(int i=0;i<words.length;i++){
+            String str=new StringBuilder(words[i]).reverse().toString();
 
-        if (hasCentralWord) count += 2;
+            if(words[i].equals(str)){
+                if (HM.get(words[i]) >= 2) {
+                    count += 4;
+                    HM.put(words[i], HM.get(words[i]) - 2);
+                } else if (!hasCenter && HM.get(words[i]) == 1) {
+                    count += 2;
+                    HM.put(words[i], HM.get(words[i]) - 1);
+                    hasCenter = true;
+                }
+            }
+
+
+            else if(HM.containsKey(words[i])&&HM.containsKey(str)&&
+                    HM.get(words[i]) > 0 && HM.get(str)>0){
+                int freqwords=HM.get(words[i]);
+                int freqstr=HM.get(str);
+                count+=4;
+                HM.put(words[i],freqwords-1);
+                HM.put(str,freqstr-1);
+            }
+        }
         return count;
     }
 }
