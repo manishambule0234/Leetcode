1c6bdb2c8d7237713bf9d39771099955df1a43ef
manishambule0234 <156496342+manishambule0234@users.noreply.github.com>
2025-05-29 11:55:54 +0530

    Time: 5 ms (64.07%), Space: 44.6 MB (47.17%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,17 +1,30 @@
 class Solution {
+    private Boolean[] t;
+    int n;
     public boolean wordBreak(String s, List<String> wordDict) {
-        boolean[] dp = new boolean[s.length() + 1];
-        dp[0] = true;
-
-        for (int i = 1; i <= s.length(); i++) {
-            for (String w : wordDict) {
-                int start = i - w.length();
-                if (start >= 0 && dp[start] && s.substring(start, i).equals(w)) {
-                    dp[i] = true;
-                    break;
-                }
+        n = s.length();
+        t = new Boolean[s.length()];
+        return solve(s, 0, wordDict);
+    }
+    
+    private boolean solve(String s, int idx, List<String> wordDict) {
+        if (idx == n) {
+            return true;
+        }
+        
+        if (t[idx] != null) {
+            return t[idx];
+        }
+        
+        for (int endIdx = idx + 1; endIdx <= n; endIdx++) {
+            
+            String split = s.substring(idx, endIdx);
+            
+            if (wordDict.contains(split) && solve(s, endIdx, wordDict)) {
+                return t[idx] = true;
             }
         }
-        return dp[s.length()];        
+        
+        return t[idx] = false;
     }
 }
